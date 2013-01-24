88df0ea546e55935a33f0ea9e03200617a1bf188
Igor Araujo <igor@dnservice.com.br>
2013-01-24 16:39:23 -0200

    gerando projeto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gibusiness::Application.config.secret_token = '214e99946fbb7d62e74d6fa1b59e5b22dcac1e1d74f1cb79e4e38a749e4f32c8fe83fc51dab4f3404364e36b86512b9c0eff3069ae2fc75b904abaefce5ea98d'
